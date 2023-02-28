598b060fdaa28be8618a76f05797027db7a26bd9
Yhordi Choque Espinoza <yordychoque124@gmail.com>
2023-02-28 18:39:15 -0400

    añadi iconos y  termine las adactaciones para que sea  compatible con dispositivos moviles

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const Close = document.getElementById('close');
+const menu= document.getElementById('menu');
+const ListaNav= document.querySelector('.nav-lista')
 
+menu.addEventListener('click',()=>{
+    ListaNav.classList.add('lista');
+    menu.style.display='none';
+    Close.style.display='block';
+});
 
+Close.addEventListener('click',Cerrar)
+
+function Cerrar(){
+    ListaNav.classList.remove('lista');
+    menu.style.display='block';
+    Close.style.display='none';
+}
